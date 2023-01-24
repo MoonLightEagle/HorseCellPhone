6d119347c4102914dc5f566792e543638a2e0e71
MoonLightEagle <danylosokoliuk13@gmail.com>
2023-01-24 02:06:47 +0200

    Added comments to code

--- T9Horse/T9Horse/Program.cs
+++ T9Horse/T9Horse/Program.cs
@@ -1,35 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 internal class Program
-{
+{       /*
+         * I upgraded solution to be more universal
+         * Now you can create a matrix of any dimention by passing height and width into constructor
+         * This will create n*m matrix with elements with values between -1 and 9, where -1 is unusable cells(obstacles,
+         * resticting horse of movement). Also now the solution not only calculates the number of unique numbers, but also
+         * store theese numbers in HashSet "uniqueNumbers".
+         * Use line 11 instead of line 15 to use premade matrix
+         * Cells cells = new Cells();
+         */
     private static void Main(string[] args)
     {
+        Cells cell = new Cells(5, 8);
 
-        Cells cells = new Cells(5, 8);
-        Console.WriteLine(cells.countDistinctNumbers(new KeyValuePair<int, int>(0, 0), 2));
-        cells.output();
-        foreach (var item in cells.uniqueNumbers)
+        // Console.WriteLine(cells.countDistinctNumbers(new KeyValuePair<int, int>(0, 0), 2));
+        Console.WriteLine(cell.countDistinctNumbers(cell));
+        cell.output();
+        foreach (var item in cell.uniqueNumbers)
         {
             Console.WriteLine(item);
         }
-        Console.WriteLine("Result is: " + cells.uniqueNumbers.Count);
+        Console.WriteLine("Result is: " + cell.uniqueNumbers.Count);
     }
 }
 
-// 1    2   3
-// 4    5   6
-// 7    8   9
-//      0
-
 class Cells
 {
+    int _height = 4;
+    int _width = 3;
+    public KeyValuePair<int, int> _staringLocation = new KeyValuePair<int, int>(0, 0);
+    public int _numberOfHops = 3;
+    public HashSet<int> uniqueNumbers = new HashSet<int>();
+
     static int[,] matrix = new int[,] { { 1, 2, 3 },
                                         { 4, 5, 6 },
                                         { 7, 8, 9 },
                                         { -1, 0, -1 } };
-    int _height = 4;
-    int _width = 3;
-    public HashSet<int> uniqueNumbers = new HashSet<int>();
+
     static readonly KeyValuePair<int, int>[] jumpps = new KeyValuePair<int, int>[] { new KeyValuePair<int, int> ( 1, 2),
                                                                    new KeyValuePair<int, int> ( 2, 1),
                                                                    new KeyValuePair<int, int> ( -1, 2),
@@ -39,13 +47,24 @@ class Cells
                                                                    new KeyValuePair<int, int> ( -1, -2),
                                                                    new KeyValuePair<int, int> ( -2,-1)};
 
+    public Cells(int n, int m, KeyValuePair<int, int> staringLocation, int numberOfHops)
+    {
+        _height = n;
+        _width = m;
+        matrix = new int[n, m];
+        randomMatrix(n, m);
+        _staringLocation = staringLocation;
+        _numberOfHops = numberOfHops;
+    }
     public Cells(int n, int m)
     {
         _height = n;
         _width = m;
         matrix = new int[n, m];
         randomMatrix(n, m);
+
     }
+    public Cells() { }
     public void output()
     {
         for (int i = 0; i < _height; i++)
@@ -58,7 +77,7 @@ class Cells
             Console.WriteLine();
         }
     }
-    public Cells() { }
+
     void randomMatrix(int n, int m)
     {
         var rand = new Random();
@@ -66,11 +85,14 @@ class Cells
         {
             for (int j = 0; j < m; j++)
             {
-                matrix[i, j] = rand.Next(10);
+                matrix[i, j] = rand.Next(-1, 10);
             }
         }
     }
-
+    public int countDistinctNumbers(Cells cell)
+    {
+        return countDistinctNumbers(cell._staringLocation, cell._numberOfHops);
+    }
     public int countDistinctNumbers(KeyValuePair<int, int> currPoint, int hopsRemaining, int prevNumber = -1)
     {
         if (prevNumber == -1)
